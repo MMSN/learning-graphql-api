8763e1d5ee72fdde28328efded80b3edadebdb77
Mateus Nascimento <mateus.msnascimento@gmail.com>
2021-11-04 19:24:43 -0300

    chore: begin first graphql project

---
+++ src/server.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type Query {
+    ola: String
+  }
+`
+
+const resolvers = {
+}
+
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Executando em ${url}`)
+})
